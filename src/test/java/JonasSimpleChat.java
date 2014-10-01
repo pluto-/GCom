9922b1cc6b4a756a641963d2083d99ccc8f8cbb9
c11jmm <c11jmm@cs.umu.se>
2014-10-01 13:47:23 +0200

    bajs

---
+++ src/test/java/JonasSimpleChat.java
@@ -0,0 +1,43 @@
+import org.jgroups.JChannel;
+
+public class JonasSimpleChat extends ReceiverAdapter {
+    JChannel channel;
+    String user_name=System.getProperty("user.name", "n/a");
+
+    private void start() throws Exception {
+        channel=new JChannel();
+        channel.setReceiver(this);
+        channel.connect("ChatCluster");
+        eventLoop();
+        channel.close();
+    }
+
+    private void eventLoop() {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            try {
+                System.out.print("> "); System.out.flush();
+                String line=in.readLine().toLowerCase();
+                if(line.startsWith("quit") || line.startsWith("exit"))
+                    break;
+                line="[" + user_name + "] " + line;
+                Message msg=new Message(null, null, line);
+                channel.send(msg);
+            }
+            catch(Exception e) {
+            }
+        }
+    }
+
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        System.out.println(msg.getSrc() + ": " + msg.getObject());
+    }
+
+    public static void main(String[] args) {
+        new JonasSimpleChat().start();
+    }
+}
