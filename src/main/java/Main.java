ab820cbf38b15facbe8fbcb0fa215476c8bf3533
Patrik Nordström <c04pnm@cs.umu.se>
2014-10-01 13:34:36 +0200

    stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import org.jgroups.JChannel;
+
 /**
  * Created by Patrik on 2014-10-01.
  */
 public class Main {
 
+    JChannel channel;
+    String user_name = System.getProperty("user.name", "n/a");
 
+    private void start() throws Exception {
+
+    }
+
+    public static final void main (String[] args) {
+
+    }
 }
---
+++ src/test/java/JonasTestKlass.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Patrik on 2014-10-01.
+ */
+public class JonasTestKlass {
+}
