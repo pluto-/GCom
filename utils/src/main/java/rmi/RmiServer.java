9e519c69e48b64ce87af6bf8f712aab32076812f
Patrik Nordström <c04pnm@cs.umu.se>
2014-10-03 14:15:42 +0200

    pakeet

--- utils/src/main/java/rmi/RmiServer.java
+++ utils/src/main/java/rmi/RmiServer.java
@@ -1,7 +1,40 @@
 package rmi;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.security.Permission;
+
 /**
  * Created by Patrik on 2014-10-03.
  */
 public class RmiServer {
+
+    Registry registry;
+
+    public RmiServer(int portNumber) throws RemoteException, UnknownHostException {
+        if(System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager() {
+                public void checkConnect (String host, int port) {}
+                public void checkConnect (String host, int port, Object context) {}
+                public void checkPermission(Permission permission) {}
+                public void checkPermission(Permission permission, Object context) {}
+                public void checkAccept(String host, int port) {}
+            });
+        }
+        System.out.println();
+        System.setProperty("java.rmi.server.hostname", Inet4Address.getLocalHost().getHostAddress());
+        System.out.println(System.getProperty("java.rmi.server.hostname"));
+        java.rmi.registry.LocateRegistry.createRegistry(portNumber);
+        registry = LocateRegistry.getRegistry();
+    }
+
+    public void bind(String name, Remote object) throws RemoteException, AlreadyBoundException {
+        registry.rebind(name, object);
+    }
+
 }
--- utils/src/test/java/rmi/RmiServerTest.java
+++ utils/src/test/java/rmi/RmiServerTest.java
@@ -1,7 +1,27 @@
 package rmi;
 
+import junit.framework.Test;
+import org.junit.Assert.*;
+import org.junit.Before;
+
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+
 /**
  * Created by Patrik on 2014-10-03.
  */
 public class RmiServerTest {
+
+
+
+    @org.junit.Test
+    public void testRemoteObject() throws Exception {
+        RmiServer server = new RmiServer(1337);
+        assert(LocateRegistry.getRegistry() != null);
+        LocateRegistry.getRegistry().bind("test", new RemoteStuffs());
+        System.out.println(LocateRegistry.getRegistry().list());
+    }
+
+    private class RemoteStuffs implements Remote {
+    }
 }
