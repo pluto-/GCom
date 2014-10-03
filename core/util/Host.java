85c65a8acb88841fb1bc4cb101c7247673c7b22f
c11jmm <c11jmm@cs.umu.se>
2014-10-03 12:02:31 +0200

    nått vackert

---
+++ core/util/Host.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.net.InetAddress;
+
+/**
+ * Created by Jonas on 2014-10-03.
+ */
+public class Host {
+
+    InetAddress address;
+
+    public Host(InetAddress address) {
+        this.address = address;
+    }
+}
---
+++ core/util/MessageTransfer.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Map;
+
+/**
+ * Created by Jonas on 2014-10-03.
+ */
+public interface MessageTransfer extends Remote {
+    public void readMessage(Host sender, String Message, Map<Host, Integer> vectorClock) throws RemoteException;
+}
