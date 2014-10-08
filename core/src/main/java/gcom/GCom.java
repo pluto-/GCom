88cb3ca8f661f5d4595d7f08ab0498a24e8b2bc5
Patrik Nordström <c04pnm@cs.umu.se>
2014-10-08 13:48:25 +0200

    delivery thread in GCom

--- core/src/main/java/gcom/GCom.java
+++ core/src/main/java/gcom/GCom.java
@@ -5,21 +5,19 @@ import gcom.groupmanager.GroupManager;
 import gcom.utils.*;
 import gcom.messagesorter.MessageSorter;
 
-import java.net.Inet4Address;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
-import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Vector;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by Jonas on 2014-10-03.
  */
-public class GCom {
+public class GCom implements Runnable {
 
     private boolean reliableMulticast;
     private RmiServer rmiServer;
@@ -27,6 +25,9 @@ public class GCom {
     private GroupManager groupManager;
     private Communicator communicator;
     private MessageSorter messageSorter;
+    private boolean running;
+    private Thread thread;
+    private BlockingQueue<String> deliveryQueue;
 
     private GComClient gcomClient;
 
@@ -37,11 +38,16 @@ public class GCom {
         this.gcomClient = gcomClient;
         groupManager = new GroupManager(nameService, rmiServer.getHost(), this);
         communicator = new Communicator(this);
-        vectorClock = new VectorClock(rmiServer.getHost());
         PeerCommunication stub = (PeerCommunication) UnicastRemoteObject.exportObject(communicator, rmiPort);
         rmiServer.bind(PeerCommunication.class.getSimpleName(), stub);
         NameServiceClient nameServiceClient = (NameServiceClient) UnicastRemoteObject.exportObject(groupManager, rmiPort);
         rmiServer.bind(NameServiceClient.class.getSimpleName(), nameServiceClient);
+
+        deliveryQueue = new LinkedBlockingQueue<>();
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
     }
 
     public void sendMessage(String text, String group, boolean sendReliably, boolean deliverCausally) throws UnknownHostException, RemoteException, NotBoundException {
@@ -79,15 +85,22 @@ public class GCom {
         communicator.sendViewChange(group.getMembers(), group.getName());
     }
 
-    public void sendLeaderElection(Group group) {
-        //TODO fix
-    }
-
     public ArrayList<Host> getGroupMembers(String groupName) {
         return groupManager.getMembers(groupName);
     }
 
     public void deliverMessage(String message) {
         gcomClient.deliverMessage(message);
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                gcomClient.deliverMessage(deliveryQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
