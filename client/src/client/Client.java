2dac829cd4637837065922ce2801778b81fadfd9
c11jmm <c11jmm@cs.umu.se>
2014-10-09 12:28:31 +0200

    Merge branch 'master' of https://github.com/pluto-/GCom
    Conflicts:
    	client/src/client/Client.java

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -44,7 +44,7 @@ public class Client implements GComClient {
             System.exit(1);
         }
         try {
-            gCom = new GCom(false, rmiPort, this, nameService, true);
+            gCom = new GCom(false, rmiPort, this, nameService);
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (UnknownHostException e) {
