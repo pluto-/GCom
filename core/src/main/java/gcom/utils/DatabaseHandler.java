2058a8f240b7c1fd46ad3892965adea8915f954c
c11jmm <c11jmm@cs.umu.se>
2014-10-28 10:40:12 +0100

    Merge branch 'master' of https://github.com/pluto-/GCom
    Conflicts:
    	core/src/main/java/gcom/GCom.java

--- core/src/main/java/gcom/utils/DatabaseHandler.java
+++ core/src/main/java/gcom/utils/DatabaseHandler.java
@@ -2,6 +2,7 @@ package gcom.utils;
 
 
 import org.apache.cassandra.service.ConsistencyLevel;
+import org.apache.commons.math3.ml.clustering.*;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
