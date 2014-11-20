44cc71e4671e51494533741b24d638ec6130cd03
c11jmm <c11jmm@cs.umu.se>
2014-11-20 11:38:38 +0100

    Merge branch 'master' of https://github.com/pluto-/GCom
    Conflicts:
    	core/src/main/java/gcom/utils/CassandraNameService.java

--- client/src/main/java/gcom/client/HoldBackQueueGUI.java
+++ client/src/main/java/gcom/client/HoldBackQueueGUI.java
@@ -5,8 +5,11 @@ import gcom.utils.Message;
 import gcom.utils.ViewChange;
 
 import javax.swing.*;
+import java.util.List;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /** If enabled, shows a new window containing information about the hold-back queue.
  * Created by Jonas on 2014-10-09.
@@ -14,12 +17,12 @@ import java.util.ArrayList;
 public class HoldBackQueueGUI extends JFrame {
 
     private JTextArea holdBackQueueText;
-    private ArrayList<Message> messages;
+    private List<Message> messages;
 
     public HoldBackQueueGUI() {
         super("Hold-Back Queue");
         setLayout(new BorderLayout());
-        messages = new ArrayList<>();
+        messages = Collections.synchronizedList(new ArrayList<Message>());
 
         JLabel label = new JLabel("Hold-Back Queue");
         holdBackQueueText = new JTextArea("");
@@ -44,25 +47,28 @@ public class HoldBackQueueGUI extends JFrame {
     }
 
     private void updateText() {
-        String text = "";
-        for(Message message : messages) {
-            if(message instanceof ViewChange) {
-                text = text.concat("\nView Change from Host: " + message.getSource());
-                text = text.concat("\n\tVector Clock");
-                for(Host member: message.getVectorClock().getClock().keySet()) {
-                    text = text.concat("\n\tHost: " + member + " Clock: " + message.getVectorClock().getValue(member));
+        synchronized(messages) {
+            String text = "";
+
+            for (Message message : messages) {
+                if (message instanceof ViewChange) {
+                    text = text.concat("\nView Change from Host: " + message.getSource());
+                    text = text.concat("\n\tVector Clock");
+                    for (Host member : message.getVectorClock().getClock().keySet()) {
+                        text = text.concat("\n\tHost: " + member + " Clock: " + message.getVectorClock().getValue(member));
+
+                    }
+                } else {
+                    text = text.concat("\nHost: " + message.getSource() + " Message: " + message.getText());
+                    text = text.concat("\n\tVector Clock");
+                    for (Host member : message.getVectorClock().getClock().keySet()) {
+                        text = text.concat("\n\tHost: " + member + " Clock: " + message.getVectorClock().getValue(member));
+
+                    }
 
                 }
-            } else {
-                text = text.concat("\nHost: " + message.getSource() + " Message: " + message.getText());
-                text = text.concat("\n\tVector Clock");
-                for(Host member: message.getVectorClock().getClock().keySet()) {
-                    text = text.concat("\n\tHost: " + member + " Clock: " + message.getVectorClock().getValue(member));
-
-                }
-
             }
+            holdBackQueueText.setText(text);
         }
-        holdBackQueueText.setText(text);
     }
 }
